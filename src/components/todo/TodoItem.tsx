4873e9245ad970dd4ff4d33de891b7637299b24a
Joey Paris <joey@leadjig.com>
2026-04-09 11:14:17 -0400

    refactor(ui): remove unused metadata badge branches

--- src/components/todo/TodoItem.tsx
+++ src/components/todo/TodoItem.tsx
@@ -25,27 +25,10 @@ function MetadataBadges({ metadata }: MetadataBadgesProps) {
           )
         }
 
-        if (key === 'location') {
-          return (
-            <span key={key} className="badge badge-default">
-              <svg className="w-3 h-3 mr-1 inline" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" /><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" /></svg>
-              {strValue}
-            </span>
-          )
-        }
-
         if (key === 'effort') {
           return (
             <span key={key} className="badge badge-default">
               <svg className="w-3 h-3 mr-1 inline" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-              {strValue}
-            </span>
-          )
-        }
-
-        if (key === 'skipability') {
-          return (
-            <span key={key} className="badge badge-default">
               {strValue}
             </span>
           )
